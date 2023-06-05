a7036fe382f46a79df0411daf3c6a75e1e4548fa
Camilo Hernandez <camilohreina.dev@gmail.com>
2023-06-05 18:37:44 -0500

    assign use state and save game storage

--- projects/02-tic-tac-toe/src/App.jsx
+++ projects/02-tic-tac-toe/src/App.jsx
@@ -4,26 +4,41 @@ import { Square } from "./components/Square.jsx";
 import { TURNS } from "./constants";
 import { checkWinnerFrom, checkEndGame } from "./logic/board";
 import { WinnerModal } from "./components/WinnerModal.jsx";
-
+import { saveGameStorage, resetGameStorage } from "./logic/storage/index.js";
 function App() {
-  const [board, setBoard] = useState(Array(9).fill(null));
-  const [turn, setTurn] = useState(TURNS.X);
+  const [board, setBoard] = useState(() => {
+    const boardFromStorage = window.localStorage.getItem("board");
+    if (boardFromStorage) return JSON.parse(boardFromStorage);
+    return Array(9).fill(null);
+  });
+  const [turn, setTurn] = useState(() => {
+    const turnFromStorage = window.localStorage.getItem("turn");
+    return turnFromStorage ?? TURNS.X;
+  });
+
+  // null es que no hay ganador, false es que hay un empate
   const [winner, setWinner] = useState(null);
 
   const resetGame = () => {
     setBoard(Array(9).fill(null));
     setTurn(TURNS.X);
     setWinner(null);
+    resetGameStorage();
   };
 
   const updateBoard = (index) => {
+    //Si ya tiene algo
     if (board[index] || winner) return;
-
+    // Actualizar el tablero
     const newBoard = [...board];
     newBoard[index] = turn;
     setBoard(newBoard);
+    //Cambiar el turno
     const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X;
     setTurn(newTurn);
+    //Guardar aquí la partida
+    saveGameStorage({ board: newBoard, turn: newTurn });
+    //Revisar si hay ganador
     const newWinner = checkWinnerFrom(newBoard);
     if (newWinner) {
       confetti();
