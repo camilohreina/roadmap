a748b30dd503a15ef471ed16879bfd3bf1a862fd
Camilo Hernandez <camilohreina.dev@gmail.com>
2023-06-08 21:05:37 -0500

    refactor custom hook use search

--- projects/05-movies-finder/src/App.jsx
+++ projects/05-movies-finder/src/App.jsx
@@ -3,26 +3,62 @@ import { useEffect, useRef, useState } from 'react'
 import { Movies } from './components/Movies'
 import { useMovies } from './hooks/useMovies'
 
+function useSearch () {
+  const [search, setSearch] = useState('')
+  const [error, setError] = useState(null)
+  const isFirstInput = useRef(true)
+
+  // Validaciones por useEffect
+  useEffect(() => {
+    if (isFirstInput.current) {
+      console.log('hola', search === '')
+      isFirstInput.current = search === ''
+      return
+    }
+
+    if (search === '') {
+      setError('No se puede buscar una película vacía')
+      return
+    }
+
+    if (search.match(/^\d+$/)) {
+      setError('no se puede buscar una película con números')
+      return
+    }
+
+    if (search.length < 3) {
+      setError('no se puede buscar una película con menos de 3 caracteres')
+      return
+    }
+
+    setError(null)
+  }, [search])
+
+  return { search, setSearch, error }
+}
+
 function App () {
   const { movies } = useMovies()
-  const [query, setQuery] = useState('')
-  const [error, setError] = useState()
+  const { search, setSearch, error } = useSearch()
 
   const handleSubmit = (event) => {
     event.preventDefault()
 
     // Se puede obtener todos los campos del form de esta manera, esto es no contolado
+
     /*  const fields = Object.fromEntries(new window.FormData(event.target))
     console.log(fields) */
 
-    console.log({ query })
+    console.log({ search })
   }
 
   const handleChange = (event) => {
     // Para obtener directamente el ultimo valor, recordar que el estado es asincrono
-    const newQuery = event.target.value
-    if (newQuery.startsWith(' ')) return
-    setQuery(newQuery)
+    /*     const newQuery = event.target.value
+    if (newQuery.startsWith(' ')) return */
+
+    setSearch(event.target.value)
+
     // Validaciones directamente cuando cambia el field
     /*     if (newQuery === '') {
       setError('no se puede buscar una película vacía')
@@ -42,32 +78,15 @@ function App () {
     setError(null) */
   }
 
-  // Validaciones por useEffect
-  useEffect(() => {
-    if (query === '') {
-      setError('no se puede buscar una película vacía')
-      return
-    }
-
-    if (query.match(/^\d+$/)) {
-      setError('no se puede buscar una película con números')
-      return
-    }
-
-    if (query.length < 3) {
-      setError('no se puede buscar una película con menos de 3 caracteres')
-      return
-    }
-
-    setError(null)
-  }, [query])
-
   return (
     <div className='page'>
       <header>
         <h1>Movies Finder</h1>
         <form className='form' onSubmit={handleSubmit}>
-          <input onChange={handleChange} value={query} name='query' placeholder='Avengers, The Matrix, Star Wars' />
+          <input
+            style={{ border: '1px solid transparent', borderColor: error ? 'red' : 'transparent' }}
+            onChange={handleChange} value={search} name='query' placeholder='Avengers, The Matrix, Star Wars'
+          />
           <button type='submit'>Search</button>
         </form>
         {error && <p style={{ color: 'red' }}>{error}</p>}
