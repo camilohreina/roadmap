9544c4f036a55bbf922a17c0b5be0e097ca766e6
Camilo Hernandez <camilohreina.dev@gmail.com>
2023-06-13 19:32:26 -0500

    only global state

--- projects/06-shopping-cart/src/components/Filters.jsx
+++ projects/06-shopping-cart/src/components/Filters.jsx
@@ -1,14 +1,12 @@
-import { useState, useId } from 'react'
+import { useId } from 'react'
 import './Filters.css'
 import { useFilters } from '../hooks/useFilters'
 
 export function Filters () {
-  const { setFilters } = useFilters()
-  const [minPrice, setMinPrice] = useState(0)
+  const { filters, setFilters } = useFilters()
   const minPriceFilterId = useId()
   const categoryFilterId = useId()
   const handleChangeMinPrice = (event) => {
-    setMinPrice(event.target.value)
     setFilters(prevState => ({
       ...prevState,
       minPrice: event.target.value
@@ -32,8 +30,9 @@ export function Filters () {
           min='0'
           max='1000'
           onChange={handleChangeMinPrice}
+          value={filters.minPrice}
         />
-        <span>${minPrice}</span>
+        <span>${filters.minPrice}</span>
       </div>
       <div>
         <label htmlFor={categoryFilterId}>Category</label>
