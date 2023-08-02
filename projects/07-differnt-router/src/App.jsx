e84f0e80d3960a6bb9a6eaa3618dd7cd28920727
Camilo Hernandez <camilohreina.dev@gmail.com>
2023-08-01 21:13:18 -0500

    route like spa

--- projects/07-differnt-router/src/App.jsx
+++ projects/07-differnt-router/src/App.jsx
@@ -1,12 +1,21 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
+
+const NAVIGATION_EVENT = 'pushstate'
+
+function navigate(href) {
+  window.history.pushState({}, '', href)
+  //Crear un evento personalizado para dar aviso del cambio 
+  const navigationEvent = new Event(NAVIGATION_EVENT)
+  window.dispatchEvent(navigationEvent)
+}
 
 function HomePage() {
   return (
     <>
       <h1>Home</h1>
       <p>Esta es una página de ejemplo para crear un React Router desde cero</p>
-      <a href='/about'>Ir a sobre nosotros</a>
+      <button onClick={() => navigate('/about')}>Ir a sobre nosotros</button>
     </>
   )
 }
@@ -20,13 +29,26 @@ function AboutPage() {
         alt='Foto de differnt'
       />
       <p>Differnt Esta creando un nuevo react router para uds</p>
-      <a href='/'>Ir a home</a>
+      <button onClick={() => navigate('/')}>Ir a home</button>
     </>
   )
 }
 
 function App() {
   const [currentPath, setCurrentPath] = useState(window.location.pathname)
+  useEffect(() => {
+    const onLocationChange = () => {
+      setCurrentPath(window.location.pathname)
+    }
+
+    //Cuando se debe ejecutar
+    window.addEventListener(NAVIGATION_EVENT, onLocationChange)
+
+    return () => {
+      window.removeEventListener(NAVIGATION_EVENT, onLocationChange)
+    }
+  }, [])
+
   return (
     <>
       {currentPath === '/' && <HomePage />}
