aa11296a581d29005089482ad194f7c028362e85
Camilo Hernandez <camilohreina.dev@gmail.com>
2023-08-11 14:47:29 -0500

    named parameters

--- projects/08-todo-app/src/App.tsx
+++ projects/08-todo-app/src/App.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { Todos } from "./components/Todos";
+import { TodoId } from "./types";
 
 const mockTodos = [
   { id: "1", title: "todo 1", completed: true },
@@ -10,7 +11,7 @@ const mockTodos = [
 const App = (): JSX.Element => {
   const [todos, setTodos] = useState(mockTodos);
 
-  const handleRemove = (id: string): void => {
+  const handleRemove = ({ id }: TodoId): void => {
     const newTodos = todos.filter((todo) => todo.id !== id);
     setTodos(newTodos);
   };
