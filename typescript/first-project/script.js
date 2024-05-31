71f70b1cb17ff5d5fb223467993a65d30f680685
Camilo Hernandez <camilohreina.dev@gmail.com>
2024-05-30 20:51:10 -0500

    clases genericas

--- typescript/first-project/script.js
+++ typescript/first-project/script.js
@@ -1,2 +1,18 @@
 "use strict";
-console.log('Se ha aprendido a compilar un archivo de TypeScript a JavaScript.');
+class Sorteo {
+    constructor(nombre) {
+        this.nombre = nombre;
+    }
+    setTicket(ticket) {
+        this.ticket = ticket;
+    }
+    getTicket() {
+        return this.ticket;
+    }
+    gambling() {
+        return `Para el sorteo ${this.nombre} el ticket ganador es: ${this.ticket}`;
+    }
+}
+const sorteo = new Sorteo('Loteria');
+sorteo.setTicket(1234);
+console.log(sorteo.gambling());
--- typescript/first-project/script.ts
+++ typescript/first-project/script.ts
@@ -1,3 +1,20 @@
-console.log(
-  'Se ha aprendido a compilar un archivo de TypeScript a JavaScript.'
-);
+class Sorteo<T> {
+  private ticket?: T;
+
+  constructor(private nombre: string) {}
+
+  setTicket(ticket: T) {
+    this.ticket = ticket;
+  }
+
+  getTicket() {
+    return this.ticket;
+  }
+  public gambling() {
+    return `Para el sorteo ${this.nombre} el ticket ganador es: ${this.ticket}`;
+  }
+}
+
+const sorteo = new Sorteo<number>('Loteria');
+sorteo.setTicket(1234);
+console.log(sorteo.gambling());
